3b269a4f1f18e1d5e80f6e1eb1e888bb6635b715
Alexandr Bereza <smailsan9@gmail.com>
2022-05-29 17:17:58 +0300

    add header-mob

---
+++ src/mob-menu.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector("[data-mob-menu-open]"),
+      closeMenuBtn: document.querySelector("[data-mob-menu-close]"),
+      menu: document.querySelector("[data-mob-menu]"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle("is-hidden");
+    }
+  })();
+  
