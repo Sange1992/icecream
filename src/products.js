d897a9d6cd40ae9a3f3a0c15648f69a770527650
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-05-31 21:28:42 +0300

    animation deleted

--- src/products.js
+++ src/products.js
@@ -12,9 +12,6 @@
     card1: document.querySelector('.first-card'),
     card2: document.querySelector('.second-card'),
     card3: document.querySelector('.third-card'),
-    item1: document.querySelector('.first-item'),
-    item2: document.querySelector('.second-item'),
-    item3: document.querySelector('.third-item'),
   };
 
   refs.openDescriptionBtn.addEventListener('click', toggleDescription);
@@ -25,18 +22,15 @@
     refs.description.classList.toggle('is-open');
     refs.btn1.classList.toggle('is-cliked');
     refs.card1.classList.toggle('is-selected');
-    refs.item1.classList.toggle('animated');
   }
   function toggleDescription2() {
     refs.description2.classList.toggle('is-open');
     refs.btn2.classList.toggle('is-cliked');
     refs.card2.classList.toggle('is-selected');
-    refs.item2.classList.toggle('animated');
   }
   function toggleDescription3() {
     refs.description3.classList.toggle('is-open');
     refs.btn3.classList.toggle('is-cliked');
     refs.card3.classList.toggle('is-selected');
-    refs.item3.classList.toggle('animated');
   }
 })();
